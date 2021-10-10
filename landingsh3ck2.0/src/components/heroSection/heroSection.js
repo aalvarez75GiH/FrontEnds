c340c517db67c74f703745e9939c2c18eceb2b27
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-10-10 11:31:42 -0400

    16th commit - adding: still working with responsive design

--- landingsh3ck2.0/src/components/heroSection/heroSection.js
+++ landingsh3ck2.0/src/components/heroSection/heroSection.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
+import useMobilDetect from '../mobilHook'
 import Video from '../../videos/video.mp4'
 import Button from '../buttons/button'
-import Image from '../../images/pexels-andrea-piacquadio-3928268.jpg'
-
+// import Image from '../../images/pexels-andrea-piacquadio-3928268.jpg'
+import Image_1 from '../../images/pexels-andrea-piacquadio-3928268.jpg'
+import Image_2 from '../../images/pexels-andrea-piacquadio-640X494.jpg'
 
 
 const HeroSection = () => {
 
+    // const [ mobil, setMobil ] = useState(null)
+    
+    // useEffect(()=> {
+    //     const itsMobil = MobilDetect()
+    //     setMobil(itsMobil)
+    //     console.log(itsMobil)
+    // },[])
+
+  const itsMobil = useMobilDetect() 
     return (
         <div className="heroContainer">
              <div className="heroBG">
                 <img
                    className="imageBG"
                    alt="this is a test"
-                   src={Image}/> 
+                //    src={Image_1}
+                   src={`${!itsMobil ? Image_1 : Image_2}`}
+                   
+                   />
+
+                   
              </div>
              <div className="heroContent">
                 <h1 className="heroH1">La nueva forma de comprar sin miedo</h1>
---
+++ landingsh3ck2.0/src/components/mobilHook.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+
+const useMobilDetect = () => {
+const [ itsMobil, setItsMobil ] = useState(null)
+
+    useEffect(()=> {
+        const mql = window.matchMedia('(min-width: 560px)')
+        mql.addEventListener("change", () => {
+            if (mql.matches){
+                setItsMobil(false)
+            }else{
+                setItsMobil(true)
+            }
+        })
+
+            
+    //     return ()=> {
+    //         removeEventListener("change")  
+    // }
+    },[])
+
+    return itsMobil
+}
+
+export default useMobilDetect
