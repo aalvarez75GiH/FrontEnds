00bd8c5e51d21b74a99532538439996fbe666b8b
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-10-17 00:35:08 -0400

    37th commit - adding: changin navBar background-color to transparent. it turns blue again when scroll happen

--- landingsh3ck2.0/src/components/navBar/navBar.js
+++ landingsh3ck2.0/src/components/navBar/navBar.js
@@ -1,25 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { Link as LinkR } from 'react-router-dom'
 import { Link as LinkS } from 'react-scroll'
 import { FaRegUser } from 'react-icons/fa'
 
 const NavBar = ({ toggleSideBar, username, login, onLogin }) => {
+    
+    
+    const [scrollNav, setScrollNav ] = useState(false)
+
+    const changeNavStyle = () => {
+        if (window.scrollY >= 80 ){
+            setScrollNav(true)
+        }else{
+            setScrollNav(false)
+        }
+    }
+    
+    useEffect(()=> {
+        window.addEventListener('scroll', changeNavStyle)
+    },[])
+
+      
     if (login){
         return(
         <>
-            <nav className="nav" >
+            <nav 
+            style={{ backgroundColor: `${ scrollNav ? '#010606' : 'transparent'}`}}
+            className="nav">           
                 <div className="navBarContainer">
                     <LinkR to="/" className="navLogo">sh3ck</LinkR>
                     <ul className="navMenu"> 
                         <li className="navItem">
-                            <LinkS to="about" className="navLinks" >Notifícame</LinkS>
+                            <LinkS
+                            style={{
+                                color: `${scrollNav ? '#ffffff' : 'transparent'}`
+                            }} 
+                            to="about" 
+                            className="navLinks" >Notifícame</LinkS>
                         </li>
                         <li className="navItem">
-                            <LinkS to="discover" className="navLinks" >Regístrate</LinkS>
+                            <LinkS
+                            style={{
+                                color: `${scrollNav ? '#ffffff' : 'transparent'}`
+                            }} 
+                            to="discover" 
+                            className="navLinks" >Regístrate</LinkS>
                         </li>
                         <li className="navItem">
-                        <LinkS to="services" className="navLinks" >Chequéa</LinkS>
+                        <LinkS
+                        style={{
+                            color: `${scrollNav ? '#ffffff' : 'transparent'}`
+                        }} 
+                        to="services" 
+                        className="navLinks" >Chequéa</LinkS>
                         </li>
                     </ul>
                     <div
@@ -39,24 +73,45 @@ const NavBar = ({ toggleSideBar, username, login, onLogin }) => {
     }else{
         return (
             <>
-                <nav className="nav" >
+                <nav 
+                style={{ backgroundColor: `${ scrollNav ? '#010606' : 'transparent'}`}}
+                className="nav">
                     <div className="navBarContainer">
                         <LinkR to="/" className="navLogo">sh3ck</LinkR>
                         <ul className="navMenu"> 
                             <li className="navItem">
-                                <LinkS to="about" className="navLinks" >Notifícame</LinkS>
+                                <LinkS 
+                                style={{
+                                    color: `${scrollNav ? '#ffffff' : 'transparent'}`
+                                }}
+                                to="about" 
+                                className="navLinks" >Notifícame</LinkS>
                             </li>
                             <li className="navItem">
-                                <LinkS to="discover" className="navLinks" >Regístrate</LinkS>
+                                <LinkS 
+                                style={{
+                                    color: `${scrollNav ? '#ffffff' : 'transparent'}`
+                                }}
+                                to="discover" 
+                                className="navLinks" >Regístrate</LinkS>
                             </li>
                             <li className="navItem">
-                                <LinkS to="services" className="navLinks" >Chequéa</LinkS>
+                                <LinkS 
+                                style={{
+                                    color: `${scrollNav ? '#ffffff' : 'transparent'}`
+                                }}
+                                to="services" 
+                                className="navLinks" >Chequéa</LinkS>
                             </li>
                         </ul>
                         <nav 
                         onClick={ onLogin }
                         className="navBtn">
-                            <LinkR to="/signin" className="navBtnLink">
+                            <LinkR 
+                            style={{
+                                border: `${scrollNav ? '2px solid #ffffff' : 'none'}`
+                            }}
+                            to="/signin" className="navBtnLink">
                                 <FaRegUser className="userIcon"/>
                                 Inicio de Sesión
                             </LinkR>
