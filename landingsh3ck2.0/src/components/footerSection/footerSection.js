383fdacd1d40b6f3b4c1738ef90f9fc0fc8c6b0a
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-11-04 12:07:45 -0400

    66th commit - changes: finishing contactInfo css style at 98%

--- landingsh3ck2.0/src/components/footerSection/footerSection.js
+++ landingsh3ck2.0/src/components/footerSection/footerSection.js
@@ -3,6 +3,23 @@ import React from 'react'
 const FooterSection = () => {
     return (
         <div className="footerSectionContainer">
+            <div className="footerWrapper">
+                <div className="footerSection1" >
+                    <div className="footerSection1_1" >
+
+                    </div>
+                    <div className="footerSection1_2">
+
+                    </div>
+                    <div className="footerSection1_3">
+
+                    </div>
+
+                </div>
+                <div className="footerSection2" >
+
+                </div>
+            </div>
             
         </div>
     )
