7f495f5e8c5a08b1004d4a1a02dcaa29eba34f53
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2022-01-17 17:18:37 -0500

    2nd commit - Changes: Uploading react-google-login-frontend

--- react-google-login-frontend/src/App.js
+++ react-google-login-frontend/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import GoogleLogin from 'react-google-login'
 
-function App() {
+const App = () =>  {
+
+  const [loginData, setLoginData ] = useState(
+    localStorage.getItem('loginData') ? JSON.parse(localStorage.getItem('loginData')) : null
+  ) 
+
+  const handleFailure = (result) => {
+    alert(result)
+  }
+
+  const handleLogin = async(googleData) => {
+    console.log(googleData)
+    const res = await fetch('/api/google-login',{
+      method: 'POST',
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json()
+    setLoginData(data)
+    localStorage.setItem('loginData', JSON.stringify(data))
+  }
+  
+  const handleLogout = () => {
+    localStorage.removeItem('loginData')
+    setLoginData(null)
+  }
+  
+  console.log(loginData)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        {
+          loginData ? (
+            <div>
+              <h3>You logged in as {loginData.name}</h3>
+              <button
+              onClick={handleLogout}
+              >Logout</button>
+            </div>
+          ):
+          <div>
+             <GoogleLogin
+             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+             buttonText="Log in with Google"
+             onSuccess={handleLogin}
+             onFailure={handleFailure}
+             cookiePolicy={'single_host_origin'}
+             />
+          </div>
+        }
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
