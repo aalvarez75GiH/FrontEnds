fbed2124cd51bdb28e90e521d9622a74d28d2d12
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2022-02-07 16:35:56 -0500

    107th commit - Changes: Including all Google sign in options and buttons from landingsh3ckFE

--- landingsh3ck2.0/src/App.js
+++ landingsh3ck2.0/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Home from './views/home'
-import GoogleAuth from './components/buttons/googleAuth'
-import GoogleAuth2 from './components/buttons/googleAuth2'
-import GoogleAuth3 from './components/buttons/googleAuth3'
+// import Home from './views/home'
+import HomeTest from './views/homeTest'
+import HomeTest2 from './views/homeTest2'
+// import GoogleAuth from './components/buttons/googleAuth'
+// import GoogleAuth2 from './components/buttons/googleAuth2'
+// import GoogleAuth3 from './components/buttons/googleAuth3'
+// import GoogleAuth4 from './components/buttons/googleAuth4'
 import './App.css'
 // import './main.css'
 
 const App = () => {
     return (
         <Router>
-            {/* <GoogleAuth2/> */}
-           <Home/> 
+            {/* <GoogleAuth/> */}
+           <HomeTest2/> 
         </Router>
         
     )
